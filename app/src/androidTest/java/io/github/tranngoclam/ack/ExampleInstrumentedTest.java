b8e3e46f3d4153298d664c29d1e0b3fcb68fa9bc
Lam Tran <tranngoclam288@gmail.com>
2017-07-25 09:23:23 +0700

    * Add custom keyboard

--- app/src/androidTest/java/io/github/tranngoclam/ack/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/github/tranngoclam/ack/ExampleInstrumentedTest.java
@@ -1,13 +1,13 @@
 package io.github.tranngoclam.ack;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +16,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.github.tranngoclam.ack", appContext.getPackageName());
-    }
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("io.github.tranngoclam.ack", appContext.getPackageName());
+  }
 }
